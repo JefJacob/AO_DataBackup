367aad950d58301a1459607b2bd698dc26df25b9
jefjvincent <43353689+jefjvincent@users.noreply.github.com>
2020-04-09 18:52:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace AO_DataBackup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started Backup Program");
+            var connectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            var backupFolder = ConfigurationManager.AppSettings["BackupFolder"];
+            try
+            {
+                BackupService bk = new BackupService(connectionString, backupFolder);
+                bk.BackupAllUserDatabases();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
